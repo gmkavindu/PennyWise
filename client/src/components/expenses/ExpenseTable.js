208e17ec305ffcaafbcbb48db04e80a595982520
kavindu <gmkavindu@gmail.com>
2024-07-28 02:12:52 +0530

    Add More User-Friendly  Search Button

--- client/src/components/expenses/ExpenseTable.js
+++ client/src/components/expenses/ExpenseTable.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { FaSearch, FaPlus } from 'react-icons/fa'; // Importing icons from react-icons/fa
-import { RiPencilLine, RiDeleteBinLine } from 'react-icons/ri'; // Importing icons from react-icons/ri
+import { FaSearch, FaPlus } from 'react-icons/fa';
+import { RiPencilLine, RiDeleteBinLine } from 'react-icons/ri';
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
 const ExpenseTable = ({ onEdit, onDelete, expenses, onAddExpense }) => {
+  // State hooks for search term, filter criteria, theme, and visibility of search fields
   const [searchTerm, setSearchTerm] = useState('');
   const [filterCategory, setFilterCategory] = useState('');
   const [filterDate, setFilterDate] = useState('');
-  const [theme, setTheme] = useState('light'); // Default theme is light
+  const [theme, setTheme] = useState('light');
+  const [showSearchFields, setShowSearchFields] = useState(false);
 
+  // Effect hook to load the theme from local storage on component mount
   useEffect(() => {
     const storedTheme = localStorage.getItem('theme');
     if (storedTheme) {
-      setTheme(storedTheme); // Set theme from localStorage
+      setTheme(storedTheme);
     }
   }, []);
 
+  // Event handlers for input changes
   const handleSearchTermChange = (e) => {
     setSearchTerm(e.target.value);
   };
@@ -29,6 +33,7 @@ const ExpenseTable = ({ onEdit, onDelete, expenses, onAddExpense }) => {
     setFilterDate(e.target.value);
   };
 
+  // Function to handle expense deletion with confirmation
   const handleDeleteClick = (id) => {
     confirmAlert({
       title: 'Confirm to delete',
@@ -37,88 +42,107 @@ const ExpenseTable = ({ onEdit, onDelete, expenses, onAddExpense }) => {
         {
           label: 'Yes',
           onClick: () => onDelete(id),
-          className: 'react-confirm-alert-button' // Use the green button style
+          className: 'react-confirm-alert-button'
         },
         {
           label: 'No',
-          className: 'react-confirm-alert-button red' // Use the red button style
+          className: 'react-confirm-alert-button red'
         }
       ]
     });
   };
 
-  // Extracting unique categories from expenses
-  const categories = [...new Set(expenses.map(expense => expense.category))];
+  // Generate a list of unique categories from the expenses
+  const categories = [...new Set(expenses.map((expense) => expense.category))];
 
+  // Filter and sort expenses based on search term, category, and date
   const filteredExpenses = expenses.filter((expense) => {
     return (
       (filterCategory === '' || expense.category === filterCategory) &&
       (filterDate === '' || new Date(expense.date).toISOString().split('T')[0] === filterDate) &&
       (searchTerm === '' || expense.description.toLowerCase().includes(searchTerm.toLowerCase()))
     );
-  }).sort((a, b) => new Date(b.date) - new Date(a.date));
+  }).sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)); // Sort by `createdAt` field
 
   return (
     <div className={`p-4 rounded shadow-lg mb-4 ${theme === 'light' ? 'bg-white text-gray-900' : 'bg-gray-800 text-white'}`}>
+      {/* Buttons to toggle search fields and add a new expense */}
       <div className="mb-4 flex flex-col sm:flex-row items-center justify-center sm:justify-between">
-        {/* Search input with icon */}
-        <div className={`relative mb-2 sm:mb-0 sm:mr-2 ${theme === 'light' ? 'bg-white' : 'bg-gray-700'} rounded-lg w-full sm:w-auto`}>
-          <input
-            type="text"
-            placeholder="Search by description"
-            value={searchTerm}
-            onChange={handleSearchTermChange}
-            className={`block w-full p-2 pl-10 pr-4 border ${theme === 'light' ? 'border-gray-300' : 'border-gray-600'} rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${theme === 'light' ? 'text-gray-900' : 'text-white'} ${theme === 'light' ? 'bg-gray-100' : 'bg-gray-600'}`}
-          />
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <FaSearch className={`${theme === 'light' ? 'text-gray-400' : 'text-gray-300'}`} />
-          </div>
-        </div>
-        {/* Category filter */}
-        <div className={`relative mb-2 sm:mb-0 sm:mr-2 ${theme === 'light' ? 'bg-white' : 'bg-gray-700'} rounded-lg w-full sm:w-auto`}>
-          <select
-            value={filterCategory}
-            onChange={handleCategoryChange}
-            className={`block w-full p-2 pl-10 pr-4 border ${theme === 'light' ? 'border-gray-300' : 'border-gray-600'} rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${theme === 'light' ? 'text-gray-900' : 'text-white'} ${theme === 'light' ? 'bg-gray-100' : 'bg-gray-600'}`}
-          >
-            <option value="">Filter by category</option>
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
-        {/* Date filter */}
-        <div className={`relative mb-2 sm:mb-0 sm:mr-2 ${theme === 'light' ? 'bg-white' : 'bg-gray-700'} rounded-lg w-full sm:w-auto`}>
-          <input
-            type="date"
-            value={filterDate}
-            onChange={handleDateChange}
-            className={`block w-full p-2 pl-10 pr-4 border ${theme === 'light' ? 'border-gray-300' : 'border-gray-600'} rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${theme === 'light' ? 'text-gray-900' : 'text-white'} ${theme === 'light' ? 'bg-gray-100' : 'bg-gray-600'}`}
-          />
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <FaSearch className={`${theme === 'light' ? 'text-gray-400' : 'text-gray-300'}`} />
-          </div>
-        </div>
-        {/* Add Expense button with + icon */}
+        <button
+          onClick={() => setShowSearchFields(!showSearchFields)}
+          className={`flex items-center justify-center mb-4 w-full sm:w-auto px-4 py-2 ${theme === 'light' ? 'bg-blue-500 text-white' : 'bg-blue-700 text-white'} rounded-full hover:bg-${theme === 'light' ? 'blue-600' : 'blue-800'}`}
+        >
+          <FaSearch className="mr-2" />
+          Search Expenses
+        </button>
         <button
           onClick={onAddExpense}
-          className={`flex items-center justify-center w-12 h-12 ${theme === 'light' ? 'bg-green-500 text-white' : 'bg-green-700 text-white'} rounded-full hover:bg-${theme === 'light' ? 'green-600' : 'green-800'}`}
+          className={`flex items-center justify-center w-12 h-12 mb-2 ${theme === 'light' ? 'bg-green-500 text-white' : 'bg-green-700 text-white'} rounded-full hover:bg-${theme === 'light' ? 'green-600' : 'green-800'}`}
         >
           <FaPlus className="text-xl" />
         </button>
       </div>
+
+      {/* Conditional rendering of search fields */}
+      {showSearchFields && (
+        <div className="mb-4 flex flex-col sm:flex-row items-center justify-center sm:justify-between space-y-2 sm:space-y-0 sm:space-x-2">
+          {/* Search by description input */}
+          <div className="relative w-full sm:w-auto">
+            <input
+              type="text"
+              placeholder="Search by description"
+              value={searchTerm}
+              onChange={handleSearchTermChange}
+              className={`block w-full p-2 pl-10 pr-4 border ${theme === 'light' ? 'border-gray-300' : 'border-gray-600'} rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${theme === 'light' ? 'text-gray-900' : 'text-white'} ${theme === 'light' ? 'bg-gray-100' : 'bg-gray-600'}`}
+            />
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <FaSearch className={`${theme === 'light' ? 'text-gray-400' : 'text-gray-300'}`} />
+            </div>
+          </div>
+          {/* Category filter dropdown */}
+          <div className="relative w-full sm:w-auto">
+            <select
+              value={filterCategory}
+              onChange={handleCategoryChange}
+              className={`block w-full p-2 pl-10 pr-4 border ${theme === 'light' ? 'border-gray-300' : 'border-gray-600'} rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${theme === 'light' ? 'text-gray-900' : 'text-white'} ${theme === 'light' ? 'bg-gray-100' : 'bg-gray-600'}`}
+            >
+              <option value="">Filter by category</option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <FaSearch className={`${theme === 'light' ? 'text-gray-400' : 'text-gray-300'}`} />
+            </div>
+          </div>
+          {/* Date filter input */}
+          <div className="relative w-full sm:w-auto">
+            <input
+              type="date"
+              value={filterDate}
+              onChange={handleDateChange}
+              className={`block w-full p-2 pl-10 pr-4 border ${theme === 'light' ? 'border-gray-300' : 'border-gray-600'} rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${theme === 'light' ? 'text-gray-900' : 'text-white'} ${theme === 'light' ? 'bg-gray-100' : 'bg-gray-600'}`}
+            />
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <FaSearch className={`${theme === 'light' ? 'text-gray-400' : 'text-gray-300'}`} />
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Conditional rendering of the table or a message if no expenses are found */}
       {filteredExpenses.length > 0 ? (
         <div className="overflow-x-auto">
           <table className="min-w-full divide-y divide-gray-200">
-            <thead className={`bg-${theme === 'light' ? 'gray-50' : 'gray-700'}`}>
+            <thead className={`${theme === 'light' ? 'bg-gray-50' : 'bg-gray-700'}`}>
               <tr>
-                <th scope="col" className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Amount</th>
-                <th scope="col" className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Category</th>
-                <th scope="col" className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Date</th>
-                <th scope="col" className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Description</th>
-                <th scope="col" className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Actions</th>
+                <th className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Amount</th>
+                <th className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Category</th>
+                <th className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Date</th>
+                <th className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Description</th>
+                <th className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider">Actions</th>
               </tr>
             </thead>
             <tbody className={`divide-y divide-gray-200 ${theme === 'light' ? 'bg-white' : 'bg-gray-800'}`}>
