5d0e57596a8427e7e9108d4bbf517db24bbe7066
kavindu <gmkavindu@gmail.com>
2024-08-05 03:36:02 +0530

    Update Expense Add and Edit To New Data Version

--- client/src/components/expenses/ExpenseForm.js
+++ client/src/components/expenses/ExpenseForm.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment, useCallback } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { confirmAlert } from 'react-confirm-alert';
 
 const ExpenseForm = ({ onSave, expenseToEdit, clearEdit, onClose }) => {
   const [amount, setAmount] = useState('');
@@ -8,15 +9,15 @@ const ExpenseForm = ({ onSave, expenseToEdit, clearEdit, onClose }) => {
   const [date, setDate] = useState('');
   const [description, setDescription] = useState('');
   const [budgets, setBudgets] = useState([]);
+  const [budgetId, setBudgetId] = useState(''); // State for storing the selected budget ID
   const [expenses, setExpenses] = useState([]);
-  const [alertMessage, setAlertMessage] = useState('');
   const [theme, setTheme] = useState('light');
-  const [loadingBudgets, setLoadingBudgets] = useState(true); // State to manage loading status
+  const [loadingBudgets, setLoadingBudgets] = useState(true);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem('theme');
     if (storedTheme) {
-      setTheme(storedTheme); // Set theme from localStorage
+      setTheme(storedTheme);
     }
   }, []);
 
@@ -32,26 +33,40 @@ const ExpenseForm = ({ onSave, expenseToEdit, clearEdit, onClose }) => {
       } catch (error) {
         console.error('Error fetching budgets:', error);
       } finally {
-        setLoadingBudgets(false); // Set loading state to false after fetching budgets (whether success or error)
+        setLoadingBudgets(false);
       }
     };
 
     fetchBudgets();
   }, []);
 
-  useEffect(() => {
-    if (category) {
-      fetchExpenses(category);
+  const fetchExpenses = useCallback(async () => {
+    if (budgetId) {
+      try {
+        const response = await axios.get(`/api/expenses?budget=${budgetId}`, {
+          headers: {
+            'x-auth-token': localStorage.getItem('token'),
+          },
+        });
+        setExpenses(response.data);
+      } catch (error) {
+        console.error('Error fetching expenses:', error);
+      }
     }
-  }, [category]);
+  }, [budgetId]);
+
+  useEffect(() => {
+    fetchExpenses();
+  }, [fetchExpenses]);
 
   useEffect(() => {
     if (expenseToEdit) {
       setAmount(expenseToEdit.amount.toString());
       setCategory(expenseToEdit.category);
+      setBudgetId(expenseToEdit.budgetId); // Set the budget ID from the expense being edited
       setDate(new Date(expenseToEdit.date).toISOString().split('T')[0]);
       setDescription(expenseToEdit.description);
-      fetchExpenses(expenseToEdit.category);
+      fetchExpenses();
     } else {
       const now = new Date();
       const year = now.getFullYear();
@@ -60,72 +75,94 @@ const ExpenseForm = ({ onSave, expenseToEdit, clearEdit, onClose }) => {
       const formattedDate = `${year}-${month}-${day}`;
       setDate(formattedDate);
     }
-  }, [expenseToEdit]);
+  }, [expenseToEdit, fetchExpenses]);
 
-  const fetchExpenses = async (category) => {
-    try {
-      const response = await axios.get(`/api/expenses?category=${category}`, {
-        headers: {
-          'x-auth-token': localStorage.getItem('token'),
-        },
-      });
-      setExpenses(response.data);
-    } catch (error) {
-      console.error('Error fetching expenses:', error);
-    }
-  };
-
-  const calculateTotalExpenses = (category, excludeId = null) => {
+  const calculateTotalExpenses = (excludeId = null) => {
     return expenses
-      .filter(exp => exp.category === category && exp._id !== excludeId)
+      .filter(exp => exp.budget === budgetId && exp._id !== excludeId)
       .reduce((total, exp) => total + exp.amount, 0);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const budgetForCategory = budgets.find((b) => b.category === category);
+    const budgetForId = budgets.find((b) => b._id === budgetId);
     const newAmount = parseFloat(amount);
 
     let newTotal;
 
     if (expenseToEdit) {
-      const totalExpenses = calculateTotalExpenses(category, expenseToEdit._id);
+      const totalExpenses = calculateTotalExpenses(expenseToEdit._id);
       newTotal = totalExpenses + newAmount;
     } else {
-      const totalExpenses = calculateTotalExpenses(category) + newAmount;
+      const totalExpenses = calculateTotalExpenses() + newAmount;
       newTotal = totalExpenses;
     }
 
-    if (budgetForCategory && newTotal > budgetForCategory.limit) {
-      const action = expenseToEdit ? 'Editing' : 'Adding';
-      setAlertMessage(`${action} this expense exceeds the budget limit for ${category}`);
-      return;
+    if (budgetForId && newTotal > budgetForId.limit) {
+      confirmAlert({
+        title: 'Budget Limit Exceeded',
+        message: `Adding this expense will exceed the budget limit for the selected budget. Do you really want to add this expense?`,
+        buttons: [
+          {
+            label: 'Cancel',
+            className: 'react-confirm-alert-button red', // Red button style
+          },
+          {
+            label: 'Edit Budget',
+            onClick: () => {
+              window.location.href = '/budgets'; // Redirect to budget page
+            },
+            className: 'react-confirm-alert-button blue', // Blue button style
+          },
+          {
+            label: 'Yes',
+            onClick: () => {
+              const now = new Date();
+              const selectedDate = new Date(date);
+              const combinedDateTime = new Date(`${selectedDate.toISOString().split('T')[0]}T${now.toTimeString().split(' ')[0]}`);
+
+              const expense = {
+                amount: newAmount,
+                category,
+                date: combinedDateTime.toISOString(),
+                description,
+                budgetId,
+                _id: expenseToEdit ? expenseToEdit._id : undefined,
+              };
+
+              onSave(expense);
+              clearEdit();
+              resetForm();
+            },
+            className: 'react-confirm-alert-button green', // Green button style
+          }
+        ]
+      });
     } else {
-      setAlertMessage(''); // Clear alert message when no error
+      const now = new Date();
+      const selectedDate = new Date(date);
+      const combinedDateTime = new Date(`${selectedDate.toISOString().split('T')[0]}T${now.toTimeString().split(' ')[0]}`);
+
+      const expense = {
+        amount: newAmount,
+        category,
+        date: combinedDateTime.toISOString(),
+        description,
+        budgetId,
+        _id: expenseToEdit ? expenseToEdit._id : undefined,
+      };
+
+      onSave(expense);
+      clearEdit();
+      resetForm();
     }
-
-    const now = new Date();
-    const selectedDate = new Date(date);
-    const combinedDateTime = new Date(`${selectedDate.toISOString().split('T')[0]}T${now.toTimeString().split(' ')[0]}`);
-    
-
-    const expense = {
-      amount: newAmount,
-      category,
-      date: combinedDateTime.toISOString(), // Use ISO string format for consistency
-      description,
-      _id: expenseToEdit ? expenseToEdit._id : undefined,
-    };
-
-    onSave(expense);
-    clearEdit();
-    resetForm();
   };
 
   const resetForm = () => {
     setAmount('');
     setCategory('');
+    setBudgetId(''); // Reset budget ID
     setDate('');
     setDescription('');
     setExpenses([]);
@@ -133,12 +170,11 @@ const ExpenseForm = ({ onSave, expenseToEdit, clearEdit, onClose }) => {
 
   const handleCancel = () => {
     clearEdit();
-    onClose(); // Close the modal on cancel
+    onClose();
   };
 
   return (
     <form onSubmit={handleSubmit} className={`p-4 rounded shadow-md ${theme === 'light' ? 'bg-white text-gray-900' : 'bg-gray-900 text-white'}`}>
-      {alertMessage && <div className="text-red-500 mb-4">{alertMessage}</div>}
       <div className="mb-4">
         <label className="block text-sm font-medium">Amount</label>
         <input
@@ -153,18 +189,22 @@ const ExpenseForm = ({ onSave, expenseToEdit, clearEdit, onClose }) => {
         <label className="block text-sm font-medium">Category</label>
         {!loadingBudgets && budgets.length > 0 ? (
           <select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className={`mt-1 p-2 block w-full border rounded focus:outline-none focus:ring-2 ${theme === 'light' ? 'focus:ring-blue-500 border-gray-300 bg-white' : 'focus:ring-blue-500 border-gray-700 bg-gray-700 text-white'}`}
-            required
-          >
-            <option value="">Select a category</option>
-            {budgets.map((budget) => (
-              <option key={budget._id} value={budget.category}>
-                {budget.category}
-              </option>
-            ))}
-          </select>
+          value={category}
+          onChange={(e) => {
+            setCategory(e.target.value);
+            const selectedBudget = budgets.find(b => b.category === e.target.value);
+            setBudgetId(selectedBudget ? selectedBudget._id : '');
+          }}
+          className={`mt-1 p-2 block w-full border rounded focus:outline-none focus:ring-2 ${theme === 'light' ? 'focus:ring-blue-500 border-gray-300 bg-white' : 'focus:ring-blue-500 border-gray-700 bg-gray-700 text-white'}`}
+          required
+        >
+          <option value="">Select a category</option>
+          {budgets.map((budget) => (
+            <option key={budget._id} value={budget.category}>
+              {budget.category}
+            </option>
+          ))}
+        </select>
         ) : (
           <div className={`mt-1 p-2 block w-full border rounded ${theme === 'light' ? 'bg-gray-100 text-gray-900' : 'bg-gray-800 text-gray-400'}`}>
             {loadingBudgets ? 'Loading budgets...' : (
