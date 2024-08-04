e8754bbd5bd78374971563bd1a3f1b1dc8017f67
kavindu <gmkavindu@gmail.com>
2024-08-05 03:43:23 +0530

    Add Feedback model and controller for managing user feedback

---
+++ server/models/Feedback.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const FeedbackMessageSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const FeedbackSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  messages: [FeedbackMessageSchema],
+});
+
+module.exports = mongoose.model('Feedback', FeedbackSchema);
