9e972211b0e24ea14d6a323e70c899f20f2bc81a
kavindu <gmkavindu@gmail.com>
2024-08-05 03:44:31 +0530

    Add new API endpoints for managing user feedback

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { addExpense, getExpenses, updateExpense, deleteExpense } = require('../controllers/expenseController');
-const { addBudget, getBudgets, updateBudget, deleteBudget } = require('../controllers/budgetController');
+const { addBudget, getBudgets, updateBudget, deleteBudget, resetBudgets } = require('../controllers/budgetController');
 const { getFinancialTips, reloadFinancialTips } = require('../controllers/tipsController');
-const { updatePersonal, updateTheme } = require('../controllers/userController');
+const { updatePersonal, updateTheme, updateUserSalary , getUserSalary, saveBudgetStatus, getLastBudgetStatus } = require('../controllers/userController');
+const { postFeedback, getFeedbacks } = require('../controllers/feedbackController');
 const User = require('../models/User'); // Import User model
 const auth = require('../middleware/auth');
 
@@ -18,6 +19,7 @@ router.post('/budgets', auth, addBudget);
 router.get('/budgets', auth, getBudgets);
 router.put('/budgets/:id', auth, updateBudget);
 router.delete('/budgets/:id', auth, deleteBudget);
+router.post('/budgets/reset', auth, resetBudgets);
 
 // Financial tips routes
 router.get('/financial-tips', auth, getFinancialTips);
@@ -26,21 +28,21 @@ router.post('/financial-tips/reload', auth, reloadFinancialTips);
 // User routes
 router.put('/user/personal', auth, updatePersonal);
 router.put('/user/theme', auth, updateTheme);
+// Get user salary details
+router.get('/salary', auth, getUserSalary);
 
-// New endpoint to get logged-in user's details
-router.get('/user/me', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+// Update user salary details
+router.put('/salary', auth, updateUserSalary);
+router.post('/user/save-budget-status', auth, saveBudgetStatus);
+router.get('/user/last-budget-status', auth, getLastBudgetStatus); // New endpoint to fetch last budget status
 
-// @route   POST /api/agree-to-privacy-policy
-// @desc    Update user agreement to privacy policy
-// @access  Private
+// Post feedback
+router.post('/feedback', auth, postFeedback);
+
+// Get all feedback
+router.get('/feedback', auth, getFeedbacks);
+
+// Update user agreement to privacy policy
 router.post('/agree-to-privacy-policy', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
